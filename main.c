c76e529f8fbd132edf52343551d07baf12104191
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-03-20 23:35:19 -0300

    FIM e ADICAO DE NOMES DOS INTERGRANTES.

--- main.c
+++ main.c
@@ -5,17 +5,27 @@ int main() {
     Grafo *G = NULL; // Grafo a ser criado
     int opcao; // Opcao do menu
 
+    printf("\n\n\t\t\t\t\t\t\tTRABALHO 01");
+    printf("\n\n\t\t\t\t\t\t\t - Grupo 10 - ");
+    printf("\n\n>> Integrantes:");
+
+    printf("\n\n\n[-] Joao Guilherme Araujo Viana");
+    printf("\n[-] Matheus Guimaraes de Oliveira");
+    printf("\n[-] Gabriel Faria de Angelis");
+    printf("\n[-] Gustavo Antonio Teixeira de Matos");
+    printf("\n[-] Hugo Alves Viana\n\n");
+
     do {
-        printf("\n\t\t --- ANALISE DE GRAFOS ---\n\n");
+        printf("\n\t\t --- ANALISE DE REDES ---\n\n");
         printf("Escolha uma opcao abaixo:\n\n");
 
         if(G == NULL) {
             printf("\n[!] Ainda nao ha nenhum grafo criado!");
             printf("\n\n1 - Criar/carregar novo grafo\n");
         } else {
-            printf("2 - Visão Geral do grafo\n");
+            printf("2 - Visao Geral do grafo\n");
             printf("3 - Inserir aresta\n");
-            printf("3 - Remover aresta\n");
+            printf("4 - Remover aresta\n");
             printf("5 - Verificar grau de um vertice\n");
             printf("6 - Verificar grau medio do grafo\n");
             printf("7 - Verificar grau maximo do grafo\n");
